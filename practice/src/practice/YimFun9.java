1ff9ca9fd705951295e1835fa74d194db8c0c3cb
Moon-Taein <mji3988@gmail.com>
2023-05-25 11:57:39 +0900

    확인

--- practice/src/practice/YimFun9.java
+++ practice/src/practice/YimFun9.java
@@ -1,29 +1,29 @@
 // 박민
 package practice;
 
-import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class YimFun9 extends JFrame {
 //	똥 싸기~!
-	
+
 	// 윈도우 빌더 테스트
 	public YimFun9() {
 		JPanel pnl = new JPanel();
 		JLabel lbl = new JLabel("^^7");
 		pnl.add(lbl);
 		add(pnl);
-		
+
 		setSize(500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		
+
 	}
+
 	public static void main(String[] args) {
 		new YimFun9(); // 다 나오나요?
+		// 잘됩니다!
 	}
-	
+
 }
